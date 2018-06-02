9c62bebe90ca7d0448857f620eb0fa9b49cd2f31
sven <13840935238@163.com>
2018-06-02 11:05:34 +0800

    add code

--- reactFoundation/demo1/src/todos/todos/views/todoList.js
+++ reactFoundation/demo1/src/todos/todos/views/todoList.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { FilterTypes } from './../../Constants';
 import * as Actions from './../Actions';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import TodoItem from './todoItem.1';
+import { createSelector } from 'reselect';
 
 const TodoList = ({ todos }) => {
-    console.log('todolist '+JSON.stringify(todos));
+    console.log('todolist ' + JSON.stringify(todos));
     return (
         <ul>
             {
                 todos.map((item) => {
                     return (
                         <TodoItem key={item.id} text={item.text} completed={item.completed}
-                        id={item.id}></TodoItem>
+                            id={item.id}></TodoItem>
                     );
                 })
             }
@@ -25,24 +26,29 @@ TodoList.propTypes = {
     todos: PropTypes.array.isRequired
 };
 
-const selectVisibleTodos = (todos, filter) => {
-    console.log('filter'+filter+todos);
+const getTodos=(state)=>state.TodosReducer;
+const getFilter=(state)=>state.FilterReducer;
+
+const selectVisibleTodos = createSelector(
+    [getTodos,getFilter],
+    (todos, filter) => {
+    console.log('filter' + filter + todos);
     switch (filter) {
         case FilterTypes.ALL:
             return todos;
         case FilterTypes.COMPLETED:
-            return todos.filter(item=>item.completed);
+            return todos.filter(item => item.completed);
         case FilterTypes.UNCOMPLETED:
-            return todos.filter(item=>!item.completed);
+            return todos.filter(item => !item.completed);
         default:
             // return 'aaa';
             throw new Error('unsupported filter');
     }
-}
+});
 
-const mapStateToProps=(state)=>{
+const mapStateToProps = (state) => {
     return {
-        todos:selectVisibleTodos(state.TodosReducer,state.FilterReducer)
+        todos: selectVisibleTodos(state)
     }
 }
 
